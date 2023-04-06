a2635748009f68a4a165817feeb8f085b0a568b6
=Irsahd <125551821+irshad0075@users.noreply.github.com>
2023-04-06 21:43:59 +0500

    updated

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,6 +1,8 @@
+// Java Practice 
+// alert("Thanks for your input!");
 
 
-// Task1 _______6-4-23______  Zakat Task
+// Task --------Zakat Task--------6-4-23
 var zakatPercentage = 0.025;
 // Here + sign convert the prompt(string) into number or we may use Number or parseInt before the prompt
 var input = +prompt("Enter your Zakat value","Your value should be in number");
