512f36189530113f293754db564bcf6f2a6a51d5
=Irsahd <125551821+irshad0075@users.noreply.github.com>
2023-04-06 19:22:50 +0500

    This is my new Project of JS

---
+++ Scripts/app.js
@@ -0,0 +1,9 @@
+
+
+// Task1 _______6-4-23______  Zakat Task
+var zakatPercentage = 0.025;
+// Here + sign convert the prompt(string) into number or we may use Number or parseInt before the prompt
+var input = +prompt("Enter your Zakat value","Your value should be in number");
+var result = zakatPercentage * input;
+ alert("Your Zakat is "+result); 
+//  upper + shows concatenation
